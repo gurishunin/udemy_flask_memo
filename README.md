348d00093d37b19d0b0225fc9d9c70ed10a6c0aa
gurishunin <gurishunin@gmail.com>
2022-08-29 01:32:44 +0900

    routing

--- README.md
+++
@@ -1,2 +0,0 @@
-# udemy_flask_memo
-Udemyコースにリリースしているプログラムです。
--- app.py
+++ app.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+@app.route("/guri")
+def hello_guri():
+    return "<p>Hello, ぐり主任!</p>"
+
+@app.route("/gura")
+def hello_gura():
+    return "<p>Hello, ぐらさん!</p>"
+
+
 if __name__ == "__main__":
     app.run()
