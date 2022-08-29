849dac760496d4fb445ee188c59b33420ccb0fa8
gurishunin <gurishunin@gmail.com>
2022-08-30 01:08:20 +0900

    templates_2

--- app.py
+++ app.py
@@ -3,9 +3,21 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_guri():
-    return render_template('index.html')
+list =[
+    'test1',
+    'test2',
+    'test3',
+    'test4',
+    'test5',
+    'test6',
+    'test7',
+    'test8',
+    'test9'
+]
+
+@app.route("/<name>")
+def hello(name):
+    return render_template('index.html',name=name,list=list)
 
 if __name__ == "__main__":
     app.run()
