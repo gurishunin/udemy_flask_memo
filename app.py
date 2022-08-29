010a2724e300bbdf56bc6f6645d60e3eaf430ec1
gurishunin <gurishunin@gmail.com>
2022-08-30 01:14:23 +0900

    top_db

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template,g
+import sqlite3
+DATABASE="flaskmemo.db"
 
 app = Flask(__name__)
 
-memo_list = [
-    {'title':"test01",'body':"ぐり主任です。"},
-    {'title':"test02",'body':"ぐらです。"},
-    #{'title':"test03",'body':"test03"}
-]
-
 @app.route("/")
 def top():
+    memo_list = get_db().execute("select id,title,body from memo").fetchall()
     return render_template('index.html',memo_list=memo_list)
 
 if __name__ == "__main__":
     app.run()
+
+#database
+def connect_db():
+    rv = sqlite3.connect(DATABASE)
+    rv.row_factory = sqlite3.Row
+    return rv
+def get_db():
+    if not hasattr(g,'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
