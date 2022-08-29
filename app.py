1f85813995547b94b9b4b90fe6c07c36c1ca7eb4
gurishunin <gurishunin@gmail.com>
2022-08-30 01:16:17 +0900

    edit

--- app.py
+++ app.py
@@ -23,6 +23,22 @@ def regist():
     
     return render_template('regist.html')
 
+@app.route("/<id>/edit",methods=['GET','POST'])
+def edit(id):
+    if request.method =='POST':
+        #画面からの登録情報の取得
+        title = request.form.get('title')
+        body = request.form.get('body')
+        db = get_db()
+        db.execute("update memo set title=?, body=? where id=?",[title,body,id])
+        db.commit()
+        return redirect('/')
+
+    post = get_db().execute(
+        "select id,title,body from memo where id=?",(id,)
+    ).fetchone()
+    return render_template('edit.html',post=post)
+
 if __name__ == "__main__":
     app.run()
 
