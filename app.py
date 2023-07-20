f044e2b1694f7c1bc196f6d9f4ab16d069846231
gurishunin <88833731+gurishunin@users.noreply.github.com>
2023-07-20 21:45:24 +0900

    Add files via upload
    最終資材への更新

--- app.py
+++ app.py
@@ -2,15 +2,85 @@ from flask import Flask
 from flask import render_template,g,redirect,request
 import sqlite3
 DATABASE="flaskmemo.db"
+from flask_login import UserMixin,LoginManager,login_required,login_user,logout_user
+import os
+from werkzeug.security import generate_password_hash,check_password_hash
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
+class User(UserMixin):
+    def __init__(self,userid):
+        self.id = userid
+##ログイン
+@login_manager.user_loader
+def load_user(userid):
+    return User(userid)
+@login_manager.unauthorized_handler
+def unauthorized():
+    return redirect('/login')
+
+@app.route("/logout",methods=['GET'])
+def logout():
+    logout_user()
+    return redirect('/login')
+
+@app.route("/signup",methods=['GET','POST'])
+def signup():
+    error_message =''
+    if request.method == 'POST':
+        userid = request.form.get('userid')
+        password = request.form.get('password')
+        pass_hash = generate_password_hash(password,method = 'sha256')
+
+        db = get_db()
+        user_check = get_db().execute("select userid from user where userid=?",[userid,]).fetchall()
+        if not user_check:
+            db.execute(
+                "insert into user (userid,password) values(?,?)",
+                [userid,pass_hash]
+            )
+            db.commit()
+            return redirect('/login')
+        else:
+            error_message ='入力されたユーザIDはすでに利用されています'
+            
+    return render_template('signup.html',error_message=error_message)
+
+
+@app.route("/login",methods=['GET','POST'])
+def login():
+    error_message =''
+    userid = ''
+
+    if request.method == 'POST':
+        userid = request.form.get('userid')
+        password = request.form.get('password')
+        #ログインのチェック
+        user_data = get_db().execute(
+            "select password from user where userid=?",[userid,]
+        ).fetchone()
+        if user_data is not None:
+            if check_password_hash(user_data[0],password):
+                user = User(userid)
+                login_user(user)
+                return redirect('/')
+        
+        error_message ='入力されたIDもしくはパスワードが誤っています'
+
+    return render_template('login.html',userid=userid,error_message=error_message)
+
+###
 @app.route("/")
+@login_required
 def top():
     memo_list = get_db().execute("select id,title,body from memo").fetchall()
     return render_template('index.html',memo_list=memo_list)
 
 @app.route("/regist",methods=['GET','POST'])
+@login_required
 def regist():
     if request.method =='POST':
         #画面からの登録情報の取得
@@ -24,6 +94,7 @@ def regist():
     return render_template('regist.html')
 
 @app.route("/<id>/edit",methods=['GET','POST'])
+@login_required
 def edit(id):
     if request.method =='POST':
         #画面からの登録情報の取得
@@ -40,6 +111,7 @@ def edit(id):
     return render_template('edit.html',post=post)
 
 @app.route("/<id>/delete",methods=['GET','POST'])
+@login_required
 def delete(id):
     if request.method =='POST':
         #画面からの登録情報の取得
