3353b294f203092782f9d65513fcafe0741316ab
gurishunin <88833731+gurishunin@users.noreply.github.com>
2023-07-20 21:50:04 +0900

    login_noDB

--- app.py
+++ app.py
@@ -2,15 +2,57 @@ from flask import Flask
 from flask import render_template,g,redirect,request
 import sqlite3
 DATABASE="flaskmemo.db"
+from flask_login import UserMixin,LoginManager,login_required,login_user,logout_user
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
+class User(UserMixin):
+    def __init__(self,userid):
+        self.id = userid
+##ログイン
+@login_manager.user_loader
+def load_user(userid):
+    return User(userid)
+@login_manager.unauthorized_handler
+def unauthorized():
+    return redirect('/login')
+
+@app.route("/logout",methods=['GET'])
+def logout():
+    logout_user()
+    return redirect('/login')
+
+@app.route("/login",methods=['GET','POST'])
+def login():
+    error_message =''
+    userid = ''
+
+    if request.method == 'POST':
+        userid = request.form.get('userid')
+        password = request.form.get('password')
+        #ログインのチェック
+        if(userid == 'guri' and password == '1234'):
+            user = User(userid)
+            login_user(user)
+            return redirect('/')
+        else:
+            error_message = '入力したIDもしくはパスワードが誤っています'            
+
+    return render_template('login.html',userid=userid,error_message=error_message)
+
+###
 @app.route("/")
+@login_required
 def top():
     memo_list = get_db().execute("select id,title,body from memo").fetchall()
     return render_template('index.html',memo_list=memo_list)
 
 @app.route("/regist",methods=['GET','POST'])
+@login_required
 def regist():
     if request.method =='POST':
         #画面からの登録情報の取得
@@ -24,6 +66,7 @@ def regist():
     return render_template('regist.html')
 
 @app.route("/<id>/edit",methods=['GET','POST'])
+@login_required
 def edit(id):
     if request.method =='POST':
         #画面からの登録情報の取得
@@ -40,6 +83,7 @@ def edit(id):
     return render_template('edit.html',post=post)
 
 @app.route("/<id>/delete",methods=['GET','POST'])
+@login_required
 def delete(id):
     if request.method =='POST':
         #画面からの登録情報の取得
